1b11e7a7cdc045225eb2d73db1ae6263bbdd7c42
Test Dami <testdami555@gmail.com>
2023-02-02 19:30:03 -0300

    Subiendo F2

---
+++ f2.py
@@ -0,0 +1,10 @@
+# Declaramos la función, que recibe el parámetro nombre
+def saludar(nombre):
+    print("Hola",nombre)
+    print("Cómo te ha tratado tu día",nombre)
+    print("Espero realmente, de corazón, que tengas una muy buena tarde",nombre)
+    print("*************************************************")    
+
+saludar("Dami")
+saludar("Coco")
+saludar("Juana")
